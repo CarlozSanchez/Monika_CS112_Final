2230ab55fab8158d09c894ed6557a9847e61b059
drakina <44460800+drakina@users.noreply.github.com>
2018-12-05 00:48:01 -0800

    Lotto Pick 3 equals method

--- src/LotteryTestDriver.java
+++ src/LotteryTestDriver.java
@@ -1,4 +1,4 @@
-4
+
 public class LotteryTestDriver
 {
 	public static void main(String[] args)
--- src/LottoPick3.java
+++ src/LottoPick3.java
@@ -1,5 +1,39 @@
 
-public class LottoPick3
-{
 
+public class LottoPick3 extends LotteryPick
+{
+	private final int  MAX_NUMBER = 3;
+	private final double COST = 1.0;
+	
+	public LottoPick3()
+	{
+		super("---000---", 0, "Lottery Pick Three");
+		getDate();
+		getCost();
+	}
+	
+	public double getCost()
+	{
+		return this.COST;
+	}
+	
+	public boolean equals(Object other)
+	{
+		if (other == null)
+		{
+			return false;
+		}
+		else if (getClass() != other.getClass())
+		{
+			return false;
+		}
+		else
+		{
+			LottoPick3 otherLotto = (LottoPick3) other;
+	// ---->		///return super.equals(otherLotto.super());
+			return false;
+		}
+		
+		
+	}
 }
